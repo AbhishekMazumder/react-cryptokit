ef99bc98085d8040b6a45291ac6f34c3df70ef87
abhishek <abhishekmazumder69@gmail.com>
2021-09-24 13:06:37 +0530

    Feat: Add the statistics part in HomePage and the Add Currencies by fetching coins

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,11 +4,19 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import Cryptocurrencies from './Cryptocurrencies';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 import News from './News';
+import Loader from './Loader';
 
 const { Title } = Typography;
 
 const Homepage = () => {
+	const { data, isFetching } = useGetCryptosQuery(10);
+	console.log(data)
+	const globalStats = data?.data?.stats;
+
+	if (isFetching) return <Loader />;
+
 	return (
 		<>
 			<Title level={2} className="heading">
@@ -16,24 +24,37 @@ const Homepage = () => {
 			</Title>
 			<Row gutter={[32, 32]}>
 				<Col span={12}>
-					<Statistic title="Total Cryptocurrencies" value={5} />
+					<Statistic title="Total Cryptocurrencies" value={globalStats.total} />
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total Exchanges" value={5} />
+					<Statistic
+						title="Total Exchanges"
+						value={millify(globalStats.totalExchanges)}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total Market Cap:" value={5} />
+					<Statistic
+						title="Total Market Cap:"
+						value={`$${millify(globalStats.totalMarketCap)}`}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total 24h Volume" value={5} />
+					<Statistic
+						title="Total 24h Volume"
+						value={`$${millify(globalStats.total24hVolume)}`}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total Cryptocurrencies" value={5} />
+					<Statistic title="Total Cryptocurrencies" value={globalStats.total} />
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total Markets" value={5} />
+					<Statistic
+						title="Total Markets"
+						value={millify(globalStats.totalMarkets)}
+					/>
 				</Col>
 			</Row>
+
 			<div className="home-heading-container">
 				<Title level={2} className="home-title">
 					Top 10 Cryptos In The World
@@ -43,6 +64,7 @@ const Homepage = () => {
 				</Title>
 			</div>
 			<Cryptocurrencies simplified />
+
 			<div className="home-heading-container">
 				<Title level={2} className="home-title">
 					Latest Crypto News
